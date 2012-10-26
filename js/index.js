e4cff93bf59050e6a83bc854d8a9005bd031ded3
ecsb <ebennett00@gmail.com>
2012-10-26 16:12:50 -0400

    development
    prototyped the bind function - iOS 5.1 doesnt have a native
    implementation

--- js/index.js
+++ js/index.js
@@ -28,6 +28,31 @@ Redsky.EDDigital.Literals.InternalTimeFormat = "HH:mm:ss";
 Redsky.EDDigital.Literals.OutputDateFormat = "yyyy MMM d";  
 Redsky.EDDigital.Literals.MobidateFormat = "MM/dd/yyyy";
 Redsky.EDDigital.Literals.StorageDateFormat = "yyyy-MM-dd";
+
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+ 
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+ 
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+ 
+    return fBound;
+  };
+}
+
 $(document).ready(function(){
 		$('#form-observation').submit(function(){Redsky.EDDigital.Events.Observation_save();return false;});
 		$('#form-login').submit(function(){Redsky.EDDigital.Events.Login_click();return false;});
